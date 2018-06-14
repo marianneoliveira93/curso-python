d108640684ac5d24c7b7e4eecaa09e8d3b19ea24
marianneoliveira93 <39701279+marianneoliveira93@users.noreply.github.com>
2018-06-14 09:52:48 -0300

    Add files via upload

---
+++ project1/fasta-file-indexing/retrieve_seq.py
@@ -0,0 +1,108 @@
+import sys
+import argparse
+from util import db_index, search_fasta
+
+
+
+
+def main():
+
+    # Option Parse
+    parser = argparse.ArgumentParser(description="A Tool to index and search large multifasta files")
+
+
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       description='valid subcommands',
+                                       help='Use retrieve_seq.py {subcommand} -h for help with each subcommand'
+                                       )
+
+
+    parser_index = subparsers.add_parser('index', help='Index all sequences in the database')
+
+    parser_index.add_argument("--db", dest='db', default=None, action="store", help="A multifasta DB to be indexed",
+                        required=False)
+
+
+    parser_extract = subparsers.add_parser('extract', help='Extract sequence in a multifasta')
+
+    parser_extract.add_argument('-f', '--file', dest='file', action="store", help="A multifasta file",
+                        required=False)
+
+    parser_extract.add_argument('-e','--end', type=int,
+                          help="end position on the fasta sequence",
+                          required=False)
+
+    parser_extract.add_argument('-s','--start', type=int,
+                          help="start position on the fasta sequence",
+                          required=False)
+
+    parser_extract.add_argument('-g','--gene', type=str,
+                          help="A gene (or chromossome) name",
+                          required=False)
+
+    parser_extract.add_argument('-l','--len', action='store_true',
+                          help="Get the length of all genes. "
+                               "If --gene get the length of the provided gene",
+                          required=False)
+
+
+
+    parser_splice = subparsers.add_parser('splice', help='extract sequence in a multifasta')
+
+    parser_splice.add_argument('-f', '--file', dest='file', action="store", help="A multifasta file",
+                                required=False)
+    parser_splice.add_argument('-r', '--range',
+                                action='store',
+                                nargs='+',
+                                help=" A values in the form start-end space separated. 10-20 50-60 70-100",
+                                required=False)
+    parser_splice.add_argument('-g', '--gene',
+                                action='store',
+                                help=" A gene (or chromossome) name",
+                                required=False)
+
+    args = parser.parse_args()
+
+
+    # function hasattr must be used because args may or may not have arg.db, and test it with just an
+    # if args.db does not work
+
+    if hasattr(args, 'db'):
+        db_index.create_index(args.db)
+        print("DB {db} has been indexed".format(db=args.db))
+
+
+    if hasattr(args, 'start') and args.start is not None:   # args.start exists and has a value
+        fasta = args.file
+        start = args.start
+        end = args.end
+        gene_name = args.gene
+        mari = search_fasta.search(fasta, start, end, gene_name)
+
+        print('>{gene}:{start}-{end}'.format(gene=gene_name,start=start,end=end))
+
+        for i in mari:
+            print(i, end='')
+        print()
+
+
+
+
+    if hasattr(args, 'len') and args.len:   # arg.len is True
+        fasta = args.file
+        gene_name = args.gene if args.gene else None
+        search_fasta.len(fasta, gene_name)
+
+    if hasattr(args, 'range'):   # arg.len is True
+        fasta = args.file
+        range = args.range
+        gene_name = args.gene
+        print('>{gene}:{range}'.format(gene=gene_name, range=range))
+        search_fasta.splice(fasta, range, gene_name)
+
+
+             # print()
+
+
+if __name__ == '__main__':
+    main()
